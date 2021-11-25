2f43f9ed550fcc973fa31a6c485e479ad0ba9dad
Mike <michael.pursell@gmail.com>
2021-11-25 19:33:41 +0000

    formatting tidy up

--- marvel-comics.py
+++ marvel-comics.py
@@ -1,10 +1,9 @@
-# reqs:
-
 """
+Requirements
 1. Ask for input from the user.
-2. Make HTTP requests to the Marvel Comics API.
+2. Make HTTP requests to the Marvel Comics API. Done
 3.Process the JSON response from the API and print out
-relevant details.
+relevant details. Done
 4.Handle the error case where no Marvel character is found
 that matches the user input.
 
@@ -15,14 +14,14 @@ import requests, json
 
 class request_handler:
 
-    url_prefix = "https://gateway.marvel.com:443/v1/public/characters?"
+    _url_prefix = "https://gateway.marvel.com:443/v1/public/characters?"
 
 
     def api_request(self, requestType, charName, apiKey):
 
         if requestType == "GET":
             self.url_suffix = "name={}&ts=1&apikey={}&hash=3a0a5532ff049374f793672544269edf".format(charName, apiKey)
-            self.url_full = self.url_prefix + self.url_suffix
+            self.url_full = self._url_prefix + self.url_suffix
             
             print("Requesting from URL:" + self.url_full)
             return requests.get(self.url_full)
@@ -40,11 +39,14 @@ class json_parser:
 
 
 def get_results():
+
     # instantiate a request_handler object and make the call
     api = request_handler()
-    response = api.api_request("GET", "hulk", "0a73c0cb4d1aa96b73be3e13bc98261c")
+    charName = input("Please enter a name of a Marvel character to look up: ")
+    publicKey = "0a73c0cb4d1aa96b73be3e13bc98261c"
+    response = api.api_request("GET", charName, publicKey )
 
-    # instantiate a json_parser object and produce a dictionary
+    # instantiate a json_parser object and produce a dictionary from the api response
     returnedJson = json_parser()
     responseDict = returnedJson.parse(response.text)
     return responseDict
@@ -58,7 +60,7 @@ def get_InfoCategory(category: str, responseDictionary):
         print (result[category])
 
 def main ():
-
+  
     resultsDictionary = get_results()
     get_InfoCategory("name", resultsDictionary)
     get_InfoCategory("description", resultsDictionary)
