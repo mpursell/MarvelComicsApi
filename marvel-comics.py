3bb0caf959f8da88de95d316f12c08fb0346b19b
Mike <michael.pursell@gmail.com>
2021-11-30 14:21:08 +0000

    comic titles and summary output now working

--- marvel-comics.py
+++ marvel-comics.py
@@ -16,6 +16,7 @@ the character appears in.
 
 import requests, json
 
+# Class to handle requests to the Marvel API
 class Request_handler:
 
     def __init__(self, apiTarget: str, query: str):
@@ -23,7 +24,7 @@ class Request_handler:
         self.query = query
         
     
-    def api_request(self, requestType, charName, apiKey):
+    def character_request(self, requestType, charName, apiKey):
 
         # url suffix and prefix generalised so that the api endpoint to query can be 
         # given at the time the object is instantiated.  
@@ -40,25 +41,38 @@ class Request_handler:
             # do some stuff with POST / UPDATE / PUT etc. 
             print("Please make a GET call")
 
+    def comics_request(self, apiKey, comicURL):
 
+        __url_prefix = comicURL
+
+        self.__url_suffix = "?&ts=1&apikey={}&hash=3a0a5532ff049374f793672544269edf".format(apiKey)
+        self.__url_full = __url_prefix + self.__url_suffix
+        
+        print("Requesting from URL:" + self.__url_full)
+        return requests.get(self.__url_full)
+            
+
+# Class to parse the JSON returned from the API into a dictionary
 class Json_parser:
 
     def parse(self, jsonContent):
         self.parsedContent = json.loads(jsonContent)
         return self.parsedContent
 
+# Class for a character creation
 class Character:
 
-    def __init__(self, name, description):
+    def __init__(self, name, description, comics):
 
         self.name = name
         self.description = description
+        self.comics = comics
 
         
 
 
-
-def get_character():
+# method to get a dictionary of character attributes given a character name
+def get_character(public_key):
 
     # instantiate a request_handler object and make the call
     characterLookup = Request_handler("characters?", "nameStartsWith")
@@ -67,8 +81,8 @@ def get_character():
     if " " in charName:
         charName = charName.replace(" ", "%20")
 
-    publicKey = "0a73c0cb4d1aa96b73be3e13bc98261c"
-    response = characterLookup.api_request("GET", charName, publicKey )
+    
+    response = characterLookup.character_request("GET", charName, public_key )
 
     # instantiate a json_parser object and produce a dictionary from the api response
     returnedJson = Json_parser()
@@ -76,42 +90,53 @@ def get_character():
     return responseDict
 
 
+# method to get a dictionary of comic attributes given a comic URL
+def get_comic(publicKey, comicURL):
 
-def get_comic(comicNumber):
+    # instantiate a request_handler object and make the api call
+    comicLookup = Request_handler("comics/?", " ")
+    response  =  comicLookup.comics_request(publicKey, comicURL)
 
-    comicNumber = comicNumber + "?"
-    comicLookup = Request_handler("comics/", comicNumber )
+    # instantiate a json_parser object and produce a dictionary from the api response
+    returnedJson = Json_parser()
+    responseDict = returnedJson.parse(response.text)
+    return responseDict
     
 
-# take the dictionary from the Json repsonse and a category name
-# find the the category name as a slice of the results: list inside the dict. 
-def get_InfoCategory(category: str, responseDictionary):
-
-    results = responseDictionary['data']['results']
-    count = responseDictionary['data']['count']
-
-    if count != 0:
-        for result in results:
-            print (result[category])
-    else:
-        print("No results for {} found".format(category))
-  
 
 def main ():
     
-    character_Dictionary = get_character()
+    publicKey = "0a73c0cb4d1aa96b73be3e13bc98261c"
+    character_Dictionary = get_character(publicKey)
 
-    results = character_Dictionary['data']['results']
+    # key into the dictionary to produce a list
+    char_results = character_Dictionary['data']['results']
     count = character_Dictionary['data']['count']
 
+    # iterate over the list, which is a list of dicts
     if count != 0:
-        for result in results:
+        for result in char_results:
+            # key into the dict as required
             name = result['name']
             description = result['description']
-            char = Character(name, description)
+            comics = result['comics']['items']
+
+            # instantiate a Character object and pass some attributes to it
+            char = Character(name, description, comics)
             print(char.name)
             print(char.description)
-    else:
+
+            for comic in char.comics:
+                comic_URL = comic['resourceURI']
+                comic_dict = get_comic(publicKey, comic_URL)
+                individualComicList = comic_dict['data']['results']
+                for individualComic in individualComicList:
+                    print(individualComic['title'])
+                    if individualComic['description'] == "":
+                        print('No summary available for this comic')
+                    else:
+                        print(individualComic['description'])
+    else:   
         print("No results found")
 
 
